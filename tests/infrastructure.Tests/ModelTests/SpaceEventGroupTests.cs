8ef55332b7e05c221b2cb9f45fa79b88617a2aaa
kennebel <jac@kennebel.com>
2019-02-10 11:35:56 -0700

    First unit tests

---
+++ tests/infrastructure.Tests/ModelTests/SpaceEventGroupTests.cs
@@ -0,0 +1,10 @@
+using System;
+using Xunit;
+
+namespace infrastructure.Tests.ModelTests
+{
+    public class SpaceEventGroupTests
+    {
+        
+    }
+}
---
+++ tests/infrastructure.Tests/ModelTests/SpaceEventTests.cs
@@ -0,0 +1,42 @@
+using System;
+using infrastructure.Models;
+using Xunit;
+
+namespace infrastructure.Tests.ModelTests
+{
+    public class SpaceEventTests
+    {
+        [Fact]
+        public void DateParts()
+        {
+            // Arrange
+            var Now = DateTime.Now;
+            var SE = new SpaceEvent() { Date = Now };
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(Now.Year, SE.Year);
+            Assert.Equal(Now.Month, SE.Month);
+            Assert.Equal(Now.Day, SE.Day);
+        }
+
+        [Fact]
+        public void ConvenienceDate()
+        {
+            // Arrange
+            var TheDate = new DateTime(1977, 5, 19);
+            var Now = DateTime.Now;
+            var SE = new SpaceEvent() { Date = TheDate };
+            
+            // Act
+            var ReturnedDate = SE.ConvenienceDate;
+            
+            // Assert
+            Assert.Equal(Now.Year, ReturnedDate.Year);
+            Assert.Equal(TheDate.Month, ReturnedDate.Month);
+            Assert.Equal(TheDate.Day, ReturnedDate.Day);
+        }
+    }
+}
